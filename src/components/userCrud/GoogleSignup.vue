5f2c05f7651b87e63d09a047237ce38de3f1db85
Talha Naeem <75784944+talhanaeem347@users.noreply.github.com>
2023-01-11 09:06:33 +0000

    GoogleSignup err handeld

--- src/components/userCrud/GoogleSignup.vue
+++ src/components/userCrud/GoogleSignup.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "@/utilites/firebase/firbase";
-const emit = defineEmits<{(e: "logedIn"): void;}>();
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+// import { auth } from "@/utilites/firebase/firbase";
+const auth = getAuth();
+const emit = defineEmits<{ (e: "logedIn"): void; }>();
 let logInWithGoogle = () => {
+
   const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider).then(() => emit("logedIn"))
+  // signInWithPopup(auth, provider).then(() => emit("logedIn"))
+  signInWithPopup(auth, provider).then((res) => {
+    GoogleAuthProvider.credentialFromResult(res);
+    emit("logedIn");
+  }).catch((err) => GoogleAuthProvider.credentialFromResult(err))
 };
 </script>
 <template>
   <section class="w-full  flex justify-center">
-    <button
-      @click="logInWithGoogle"
-      class="text-center border w-fit px-2 rounded-md my-4 pb-1 bg-gray-100 hover:text-blue-400"
-    >
+    <button @click="logInWithGoogle"
+      class="text-center border w-fit px-2 rounded-md my-4 pb-1 bg-gray-100 hover:text-blue-400">
       Continue With Google
       <span class="text-xl text-blue-500 italic font-extrabold">G</span>
     </button>
